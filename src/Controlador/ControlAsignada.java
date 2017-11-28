ff43bd376cc42da0ad5db9a672deb2ea61540020
juandavid <juandavid@TOSHIBA.zte.com.cn>
2017-11-27 23:13:45 -0500

    controlasiganada creada

---
+++ src/Controlador/ControlAsignada.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import AccesoDatos.DaoAsignada;
+import Modelo.Asignada;
+
+/**
+ *
+ * @author juandavid
+ */
+public class ControlAsignada {
+     DaoAsignada daoAsignada;
+    
+    public ControlAsignada(){
+        daoAsignada = new DaoAsignada();
+    }
+    
+     public String insertarAsignada(String id_persona, String num_cama, String fecha_asignacion, String fecha_finalizacion){
+        Asignada a  = new Asignada(id_persona, num_cama, fecha_asignacion, fecha_finalizacion);
+        return daoAsignada.guardarAsignada(a);
+    }
+    
+    public String[] consultarAsignada(String num_cama){
+        return daoAsignada.consultarAsignada(num_cama);
+    }
+    
+    public String modificarAsignada(String id_persona, String num_cama, String fecha_asignacion, String fecha_finalizacion){
+        Asignada a = new Asignada(id_persona, num_cama, fecha_asignacion, fecha_finalizacion);
+        return daoAsignada.modificarAsignada(a);
+    }
+    
+    public String eliminarAsignada(String num_cama){
+        return daoAsignada.eliminarAsignada(num_cama);
+    }
+    
+    public void cerrarConexionBD(){
+        daoAsignada.cerrarConexionBD();
+    }
+}
