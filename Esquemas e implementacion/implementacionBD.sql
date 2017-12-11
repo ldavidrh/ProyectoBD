10df401641801831bff8881562afb87287ea041e
sophiepsist <diananavas27@gmail.com>
2017-12-10 23:47:01 -0500

    update insert medicamento

--- Esquemas e implementacion/implementacionBD.sql
+++ Esquemas e implementacion/implementacionBD.sql
@@ -238,7 +238,7 @@ INSERT INTO registro VALUES ('01','1000', '333', '16-10-2017');
 
 INSERT INTO cita VALUES ('333','111', '31-12-2017','5:30', 50000);
 
-INSERT INTO medicamento VALUES ('1212','Dolex', 'Ayuda contra el malestar general', 700);
+INSERT INTO medicamento VALUES ('1212','Dolex', 'Ayuda contra el malestar general', 700, 1);
 
 INSERT INTO formula VALUES ('0001','1212', '333', '111');
 
